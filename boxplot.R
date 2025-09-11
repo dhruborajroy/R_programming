110d824e0afc8e79e90ef737c62dc675353b6541
Dhrubo <dhruborajroy3@gmail.com>
2025-09-11 13:44:54 +0600

    density plot added

--- boxplot.R
+++ boxplot.R
@@ -1,8 +1,54 @@
 #one categorical and one numerical 
-
+# the standard practice to plot a box plot that the categorical
+# variables should in y axis and the numerical variables are in x axis. 
+#beacuse if the variables are in large in size it is easier to learn the y axis text.
 library(tidyverse)
 
 
 data()
 
+msleep
+head(msleep)
 
+
+msleep %>% 
+  drop_na(vore) %>% 
+  ggplot(aes(x=vore,y=sleep_total))+
+  geom_boxplot()+
+  theme_bw()+
+  labs(title="box plot")+
+  coord_flip()+
+  facet_wrap(~vore)
+
+msleep %>% 
+  drop_na(vore) %>% 
+  ggplot(aes(sleep_total))+
+  geom_boxplot()+
+  theme_bw()+
+  labs(title="box plot")+
+  coord_flip()+
+  facet_wrap(~vore)
+
+
+msleep %>% 
+  drop_na(vore) %>% 
+  ggplot(aes(sleep_total))+
+  geom_density()+
+  theme_bw()+
+  labs(title="box plot")+
+  #coord_flip()+
+  facet_wrap(~vore)
+
+
+#histogram
+msleep %>% 
+  drop_na(vore) %>% 
+  ggplot(aes(sleep_total))+
+  geom_histogram()+
+  theme_bw()+
+  labs(title="box plot")+
+  #coord_flip()+
+  facet_wrap(~vore)
+
+
+summary(msleep$sleep_total)  
--- density plot.R
+++ density plot.R
@@ -1,3 +1,55 @@
 # one categorical variable & one numerical varible 
+#density plots are more suitable for fraction data and the histograms are more suitable for full data.
+
+library(tidyverse)
 
 
+data()
+
+msleep
+head(msleep)
+
+
+msleep %>% 
+  drop_na(vore) %>% 
+  ggplot(aes(x=vore,y=sleep_total))+
+  geom_boxplot()+
+  theme_bw()+
+  labs(title="box plot")+
+  coord_flip()+
+  facet_wrap(~vore)
+
+msleep %>% 
+  drop_na(vore) %>% 
+  ggplot(aes(sleep_total))+
+  geom_boxplot()+
+  theme_bw()+
+  labs(title="box plot")+
+  coord_flip()+
+  facet_wrap(~vore)
+
+
+msleep %>% 
+  filter(vore=="herbi" | vore=="omni") %>% 
+  filter(vore %in% c("carni", "omni")) %>% 
+  drop_na(vore) %>% 
+  ggplot(aes(sleep_total,color=vore,fill=vore))+
+  geom_density(alpha=0.2)
+  theme_bw()+
+  labs(title="box plot")
+  #coord_flip()+
+  #facet_wrap(~vore)
+
+
+#histogram
+msleep %>% 
+  drop_na(vore) %>% 
+  ggplot(aes(sleep_total))+
+  geom_histogram()+
+  theme_bw()+
+  labs(title="box plot")
+  #coord_flip()+
+  #facet_wrap(~vore)
+
+
+summary(msleep$sleep_total)  
